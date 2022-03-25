6c75200ec458f1f6abc0ca972482ca2a0fd6620f
Natalia <natalia.s36.ns@gmail.com>
2022-03-25 11:43:55 -0300

    Adicionando footer e navbar.

---
+++ src/componentes/estaticos/navbar/Navbar.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
+import '../navbar/Navbar.css';
+function Navbar() {
+    return (
+        <>
+            <AppBar className='corMenu' position="static">
+                <Toolbar variant="dense">
+                    <Box style={{ cursor: "pointer" }} >
+                        <Typography variant="h5" color="inherit">
+                            LearnPointLogo
+                        </Typography>
+                    </Box>
+
+                    <Box display="flex" justifyContent="start">
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                                Home
+                            </Typography>
+                        </Box>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                                Entre em contato
+                            </Typography>
+                        </Box>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                                Fazer login
+                            </Typography>
+                        </Box>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                                Visualizar produtos
+                            </Typography>
+                        </Box>
+                        <Box mx={1} style={{ cursor: "pointer" }}>
+                            <Typography variant="h6" color="inherit">
+                               Sair
+                            </Typography>
+                        </Box>
+                    </Box>
+
+                </Toolbar>
+            </AppBar>
+        </>
+    )
+}
+
+export default Navbar;
