7e0b4718bab45c572f3688762c2e89b8e490ce16
Denise <denisekellypereira@hotmail.com>
2022-03-25 13:42:24 -0300

    Adicionando a Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import Footer from './componentes/estaticos/footer/Footer';
 import Navbar from './componentes/estaticos/navbar/Navbar';
+import Home from './paginas/home/Home'
+import Login from './paginas/login/Login';
 import { Grid } from '@material-ui/core';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Navbar/>
+     <Router>
+      <Navbar />
       <Switch>
-      <div>
-
-        <Route>
-        
-
-        </Route>
-     </div>
+        <div style={{minHeight: '100vh'}}>
+        <Route exact path='/'>
+            <Login/>
+          </Route>
+        <Route path='/login'>
+            <Login />
+          </Route>
+          <Route path='/home'>
+            <Home />
+          </Route>
+        </div>
       </Switch>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
