10429bb581417c13e3391ea0dc331629d6af75bf
Domenica Araujo <domenica.araujo65@gmail.com>
2022-04-05 13:02:37 -0300

    service.ts

--- src/service/Service.ts
+++ src/service/Service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://learnpointpi.herokuapp.com'
+    baseURL: 'https://learnpointpi.herokuapp.com/'
 })
 
 export const cadastroUsuario = async (url: any, dados: any, setDado: any) => {
@@ -11,4 +11,27 @@ export const cadastroUsuario = async (url: any, dados: any, setDado: any) => {
 export const login = async (url: any, dados: any, setDado: any) => {
     const resposta = await api.post(url, dados)
     setDado(resposta.data.token)
+}
+export const busca = async(url: any, setDados: any, header: any) => {
+    const resposta = await api.get(url, header)
+    setDados(resposta.data)
+}
+
+export const buscaId = async(url: any, setDados: any, header: any) => {
+    const resposta = await api.get(url, header)
+    setDados(resposta.data)
+}
+
+export const post = async(url: any, dados: any, setDados: any, header: any) => {
+    const resposta = await api.post(url, dados, header)
+    setDados(resposta.data)
+}
+
+export const put = async(url: any, dados: any, setDados: any, header: any) => {
+    const resposta = await api.put(url, dados, header)
+    setDados(resposta.data)
+}
+
+export const deleteId = async(url: any, header: any) => {
+    await api.delete(url, header)
 }
